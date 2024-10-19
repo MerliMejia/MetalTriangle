724de8aaa2bcb7f4d5956d69c6633b38043e2538
Merli Mejia <merlimejia25@gmail.com>
2024-10-18 22:55:34 -0400

    Initial Commit

---
+++ MetalTriangle/MetalTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTriangleApp.swift
+//  MetalTriangle
+//
+//  Created by Merli Mejia on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
